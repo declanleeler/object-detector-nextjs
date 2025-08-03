ef2fce4813ab499eab7c4b4d7c30cb18b7aa12e2
declanleeler <declanlee-ler@hotmail.com>
2025-08-03 11:04:58 +0800

    Shift folder out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import BackgroundBlob from "@/components/BackgroundBlob";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
-import Providers from "./context/Providers";
+import Providers from "../context/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
